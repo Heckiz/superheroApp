bc5fd83b70144a0feacae66c33e24d3f75152229
Fabián Romero <fabo.romero97@gmail.com>
2021-09-10 02:20:57 -0300

    implementation of async-storage + redux-persit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React, {FC} from 'react';
 import {Provider} from 'react-redux';
-import {store} from './src/app/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor, store} from './src/app/store';
 import MainNav from './src/navigation/mainNav';
 
 const App: FC = () => {
   return (
     <>
       <Provider store={store}>
-        <MainNav />
+        <PersistGate loading={null} persistor={persistor}>
+          <MainNav />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,28 @@
 import {configureStore} from '@reduxjs/toolkit';
 import superherosReducer from './superheroSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+import {PERSIST} from 'redux-persist/es/constants';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, superherosReducer);
 
 export const store = configureStore({
   reducer: {
-    superheros: superherosReducer,
+    superheros: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
